4516bdf4d9a5c4f890dd44bafb27f1e7c82bc875
nedu'm <davidsonchinedum@gmail.com>
2022-05-27 12:40:47 +0100

    types and coercion

--- learn.js
+++ learn.js
@@ -5,3 +5,13 @@ var x = null
 
 var v = 42n
 //console.log(typeof v) // "bigint"
+
+//Negative zero
+function formatTrend(trendRate) {
+  var direction =
+    (trendRate < 0 || Object.is(trendRate, -0)) ? "⬇" : "⬆";
+  return `${direction} ${Math.abs(trendRate)}`
+}
+
+// console.log(formatTrend(-3))
+// console.log(formatTrend(-0))
