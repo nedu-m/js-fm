6422fd31a8cd90bc531891b2718f6228ac33ebca
nedu'm <davidsonchinedum@gmail.com>
2022-06-07 20:52:31 +0100

    OLOO programming

--- this.js
+++ this.js
@@ -73,3 +73,50 @@ console.log(deepJS.__proto__ === Workshop.prototype)
 console.log(Object.getPrototypeOf(deepJS) === Workshop.prototype)
 
 //Objects link to other objects
+var FirstWorkshop = {
+  setTeacher(teacher) {
+    this.teacher = teacher
+  },
+  ask(question) {
+    console.log(this, question)
+  }
+};
+
+var SecondWorkshop = Object.assign(
+  Object.create(FirstWorkshop),
+  {
+    speakUp(msg) {
+      this.ask(msg.toUpperCase());
+    }
+  }
+);
+
+var JSRecentParts = Object.create(SecondWorkshop)
+JSRecentParts.setTeacher("Quincy")
+JSRecentParts.speakUp("Is this OLOO?")
+
+//Using Delegation to solve Login & Authentication issues
+var AuthController = {
+  authenticate() {
+    server.authenticate(
+      [this.username, this.password],
+      this.handleResponse.bind(this)
+    );
+  },
+  handleResponse(resp) {
+    if (!resp.ok) this.displayError(resp.msg);
+  }
+};
+
+var LoginFormController = Object.assign(
+  Object.create(AuthController), {
+  onsubmit() {
+    this.username = this.$username.val();
+    this.password = this.$password.val();
+    this.authenticate();
+  },
+  displayError(msg) {
+    alert(msg)
+  }
+}
+);
